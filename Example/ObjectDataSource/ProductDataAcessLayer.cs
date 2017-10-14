e8b9c3312c829e4f2c414fc8b2486ca77a82f00b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-14 23:02:47 +0600

    Grid View

---
+++ Example/ObjectDataSource/ProductDataAcessLayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace ObjectDataSource
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }   
+    }
+    public class ProductDataAcessLayer
+    {
+        public static List<Product> GetAllProduct()
+        {
+            List<Product>listItem= new List<Product>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con= new SqlConnection(CS);
+            SqlCommand cmd= new SqlCommand("select * from Products", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Product product= new Product();
+                product.Id = Convert.ToInt32(rdr["Id"]);
+                product.Name = rdr["Name"].ToString();
+                product.Description = rdr["Description"].ToString();
+                listItem.Add(product);
+            }
+            return listItem;
+        }
+    }
+}
---
+++ Example/ObjectDataSource/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ObjectDataSource.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="ObjectDataSource1">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id" SortExpression="Id" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+            </Columns>
+        </asp:GridView>
+        <br/>
+        <br/>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" SelectMethod="GetAllProduct" TypeName="ObjectDataSource.ProductDataAcessLayer"></asp:ObjectDataSource>
+    </div>
+    </form>
+</body>
+</html>
