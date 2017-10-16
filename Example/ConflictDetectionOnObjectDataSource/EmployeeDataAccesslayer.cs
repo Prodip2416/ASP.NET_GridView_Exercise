fc1ba8fbf05e0a18c68e6b81c2a641acf30e8e85
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-17 01:03:27 +0600

    Grid View

---
+++ Example/ConflictDetectionOnObjectDataSource/EmployeeDataAccesslayer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ConflictDetectionOnObjectDataSource
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+    }
+
+    public class EmployeeDataAccesslayer
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("select * from tblEmployee", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Employee employee = new Employee();
+                employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                employee.EmployeeName = rdr["Name"].ToString();
+                employee.Gender = rdr["Gender"].ToString();
+                employee.Salary = Convert.ToInt32(rdr["salary"]);
+                employees.Add(employee);
+            }
+            con.Close();
+            return employees;
+        }
+
+        public static void DeleteEmployee(int original_EmployeeId, string original_Name,
+            string original_Gender, string original_City)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string deleteQuery = "DELETE FROM tblEmployee WHERE EmployeeId = @original_EmployeeId " +
+                                     "AND Name = @original_Name AND Gender = @original_Gender AND City = @original_City";
+                SqlCommand cmd = new SqlCommand(deleteQuery, con);
+                SqlParameter paramEmployeeId = new SqlParameter("@original_EmployeeId", original_EmployeeId);
+                cmd.Parameters.Add(paramEmployeeId);
+                SqlParameter paramName = new SqlParameter("@original_Name", original_Name);
+                cmd.Parameters.Add(paramName);
+                SqlParameter paramGender = new SqlParameter("@original_Gender", original_Gender);
+                cmd.Parameters.Add(paramGender);
+                SqlParameter paramCity = new SqlParameter("@original_City", original_City);
+                cmd.Parameters.Add(paramCity);
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
---
+++ Example/ConflictDetectionOnObjectDataSource/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ConflictDetectionOnObjectDataSource.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2" DataSourceID="ObjectDataSource1">
+            <Columns>
+                <asp:CommandField ShowDeleteButton="True" />
+                <asp:BoundField DataField="EmployeeId" HeaderText="EmployeeId" SortExpression="EmployeeId" />
+                <asp:BoundField DataField="EmployeeName" HeaderText="EmployeeName" SortExpression="EmployeeName" />
+                <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                <asp:BoundField DataField="Salary" HeaderText="Salary" SortExpression="Salary" />
+            </Columns>
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FFF1D4" />
+            <SortedAscendingHeaderStyle BackColor="#B95C30" />
+            <SortedDescendingCellStyle BackColor="#F1E5CE" />
+            <SortedDescendingHeaderStyle BackColor="#93451F" />
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ObjectDataSource1" runat="server"
+                              ConflictDetection="CompareAllValues" OldValuesParameterFormatString="original_{0}"
+                              DeleteMethod="DeleteEmployee" SelectMethod="GetAllEmployees"
+             TypeName="ConflictDetectionOnObjectDataSource.EmployeeDataAccesslayer">
+            <DeleteParameters>
+                <asp:Parameter Name="original_EmployeeId" Type="Int32" />
+                <asp:Parameter Name="original_Name" Type="String" />
+                <asp:Parameter Name="original_Gender" Type="String" />
+                <asp:Parameter Name="original_City" Type="String" />
+            </DeleteParameters>
+        </asp:ObjectDataSource>
+    </div>
+    </form>
+</body>
+</html>
