36b017b5402baf8667d54289a00111fa8bdda51b
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-17 01:38:30 +0600

    Grid View

---
+++ Example/EditUpdatingOnObjectDataSource/EmployeeDataAccessLayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EditUpdatingOnObjectDataSource
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+
+    public class EmployeeDataAccessLayer
+    {
+        public static void UpdateEmployee(int EmployeeId, string Name,
+                                    string Gender, string City)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string updateQuery = "Update Employee4 SET Name = @Name,  " +
+                    "Gender = @Gender, City = @City WHERE EmployeeId = @EmployeeId ";
+                SqlCommand cmd = new SqlCommand(updateQuery, con);
+                SqlParameter paramEmployeeId = new SqlParameter("@EmployeeId", EmployeeId);
+                cmd.Parameters.Add(paramEmployeeId);
+                SqlParameter paramName = new SqlParameter("@Name", Name);
+                cmd.Parameters.Add(paramName);
+                SqlParameter paramGender = new SqlParameter("@Gender", Gender);
+                cmd.Parameters.Add(paramGender);
+                SqlParameter paramCity = new SqlParameter("@City", City);
+                cmd.Parameters.Add(paramCity);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employee4", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                   employee.City = rdr["City"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return listEmployees;
+        }
+
+    }
+}
---
+++ Example/EditUpdatingOnObjectDataSource/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EditUpdatingOnObjectDataSource")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EditUpdatingOnObjectDataSource")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("816f4ea9-442e-4eb0-8f16-754b696b374e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
