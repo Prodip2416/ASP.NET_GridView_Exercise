679ca600f6b1eacefaa941b679fc5bd877db6aac
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-22 01:59:12 +0600

    GridView

---
+++ Example/DisplayingImageInGridView/WebForm1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DisplayingImageInGridView
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataSource = EmplyeeDataAccessLayer.GetAllEmployees();
+            GridView1.DataBind();
+        }
+    }
+}
