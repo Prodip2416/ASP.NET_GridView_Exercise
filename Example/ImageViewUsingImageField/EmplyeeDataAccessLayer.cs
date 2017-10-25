3db723bccec9b05abf5d62f89f2e405803cf2191
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 00:12:41 +0600

    Grid View

---
+++ Example/ImageViewUsingImageField/EmplyeeDataAccessLayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DisplayingImageInGridView
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Photo { get; set; }
+    }
+    public class EmplyeeDataAccessLayer
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employee5", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.City = rdr["City"].ToString();
+                    employee.Photo = rdr["PhotoPath"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return listEmployees;
+        }
+    }
+}
