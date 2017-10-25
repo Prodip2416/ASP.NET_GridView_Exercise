fbedfd0dceb2e57bca3a1abb60fb16ee2cf8024d
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:15:44 +0600

    Grid View

---
+++ Example/SortingGridViewAscendingDescending/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SortingGridViewWithOutDataSource
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridView1.DataSource = EmployeeDataAccessLayer.GetAllEmployees("EmployeeId");
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            SortDirection sortDirection = SortDirection.Ascending;
+            string sortField = string.Empty;
+
+            SortGridview((GridView)sender, e, out sortDirection, out sortField);
+            string strSortDirection = sortDirection == SortDirection.Ascending ? "ASC" : "DESC";
+
+
+            GridView1.DataSource = EmployeeDataAccessLayer.GetAllEmployees
+                (e.SortExpression + " " + strSortDirection);
+            GridView1.DataBind();
+        }
+        private void SortGridview(GridView gridView, GridViewSortEventArgs e, out SortDirection sortDirection, out string sortField)
+        {
+            sortField = e.SortExpression;
+            sortDirection = e.SortDirection;
+
+            if (gridView.Attributes["CurrentSortField"] != null && gridView.Attributes["CurrentSortDirection"] != null)
+            {
+                if (sortField == gridView.Attributes["CurrentSortField"])
+                {
+                    if (gridView.Attributes["CurrentSortDirection"] == "ASC")
+                    {
+                        sortDirection = SortDirection.Descending;
+                    }
+                    else
+                    {
+                        sortDirection = SortDirection.Ascending;
+                    }
+                }
+
+                gridView.Attributes["CurrentSortField"] = sortField;
+                gridView.Attributes["CurrentSortDirection"] = (sortDirection == SortDirection.Ascending ? "ASC" : "DESC");
+            }
+        }
+    }
+}
