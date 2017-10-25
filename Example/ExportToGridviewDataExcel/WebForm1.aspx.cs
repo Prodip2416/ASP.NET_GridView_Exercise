d726b76f1d123dbeaa91015ea36114829d3b549c
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:44:51 +0600

    Grid View

---
+++ Example/ExportToGridviewDataExcel/WebForm1.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExportToGridviewDataExcel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData();
+            }
+        }
+        private void BindData()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from Employee4", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+        protected void Button1_OnClick(object sender, EventArgs e)
+        {
+            // Clear all content output from the buffer stream
+            Response.ClearContent();
+            // Specify the default file name using "content-disposition" RESPONSE header
+            Response.AppendHeader("content-disposition", "attachment; filename=Employees.xls");
+            // Set excel as the HTTP MIME type
+            Response.ContentType = "application/excel";
+            // Create an instance of stringWriter for writing information to a string
+            System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+            // Create an instance of HtmlTextWriter class for writing markup 
+            // characters and text to an ASP.NET server control output stream
+            HtmlTextWriter htw = new HtmlTextWriter(stringWriter);
+
+            // Set white color as the background color for gridview header row
+            GridView1.HeaderRow.Style.Add("background-color", "#FFFFFF");
+
+            // Set background color of each cell of GridView1 header row
+            foreach (TableCell tableCell in GridView1.HeaderRow.Cells)
+            {
+                tableCell.Style["background-color"] = "#A55129";
+            }
+
+            // Set background color of each cell of each data row of GridView1
+            foreach (GridViewRow gridViewRow in GridView1.Rows)
+            {
+                gridViewRow.BackColor = System.Drawing.Color.White;
+                foreach (TableCell gridViewRowTableCell in gridViewRow.Cells)
+                {
+                    gridViewRowTableCell.Style["background-color"] = "#FFF7E7";
+                }
+            }
+
+            GridView1.RenderControl(htw);
+            Response.Write(stringWriter.ToString());
+            Response.End();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {        
+            Response.ClearContent();
+            Response.AppendHeader("content-disposition", "attachment; filename=Employees.doc");      
+            Response.ContentType = "application/word";
+
+            StringWriter stringWriter = new StringWriter();        
+            HtmlTextWriter htw = new HtmlTextWriter(stringWriter);
+
+
+            GridView1.HeaderRow.Style.Add("background-color", "#FFFFFF");
+
+
+            foreach (TableCell tableCell in GridView1.HeaderRow.Cells)
+            {
+                tableCell.Style["background-color"] = "#A55129";
+            }
+
+            // Set background color of each cell of each data row of GridView1
+            foreach (GridViewRow gridViewRow in GridView1.Rows)
+            {
+                gridViewRow.BackColor = System.Drawing.Color.White;
+                foreach (TableCell gridViewRowTableCell in gridViewRow.Cells)
+                {
+                    gridViewRowTableCell.Style["background-color"] = "#FFF7E7";
+                }
+            }
+
+            GridView1.RenderControl(htw);
+            Response.Write(stringWriter.ToString());
+            Response.End();
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+           
+        }
+    }
+}
