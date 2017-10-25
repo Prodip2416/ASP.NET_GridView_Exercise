5fecd7c54c9e12b8f99fbc32a4aff91e0d50ad68
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:50:21 +0600

    Grid View

---
+++ Example/GeneratePDFdataandStoreOnWenServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeneratePDFdataandStoreOnWenServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeneratePDFdataandStoreOnWenServer")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c290ec6c-6c95-4b7c-8b59-5b4f73ad369b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Example/GeneratePDFdataandStoreOnWenServer/WebForm1.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ExportGridviewDataToPDF
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                SqlConnection con= new SqlConnection(CS);
+                SqlDataAdapter da= new SqlDataAdapter("select * from Employee4", con);
+                DataSet ds= new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            PdfPTable pdfPTable= new PdfPTable(GridView1.HeaderRow.Cells.Count);
+
+            //addind header row........
+              foreach (TableCell headerCell in GridView1.HeaderRow.Cells)
+                {
+                    Font font= new Font();
+                    font.Color=new BaseColor(GridView1.HeaderStyle.ForeColor);
+
+                    PdfPCell pdfPCell = new PdfPCell(new Phrase(headerCell.Text));
+                   pdfPCell.BackgroundColor=new BaseColor(GridView1.HeaderStyle.BackColor);
+                    pdfPTable.AddCell(pdfPCell);
+                }
+         
+           // adding cell...............
+            foreach (GridViewRow gridViewRow in GridView1.Rows)
+            {
+                foreach (TableCell tableCell in gridViewRow.Cells)
+                {
+                    Font font = new Font();
+                    font.Color = new BaseColor(GridView1.RowStyle.ForeColor);
+
+                    PdfPCell pdfPCell= new PdfPCell(new Phrase(tableCell.Text));
+                    pdfPCell.BackgroundColor = new BaseColor(GridView1.RowStyle.BackColor);
+                    pdfPTable.AddCell(pdfPCell);
+                }
+            }
+
+            Document pdfdocument= new Document(PageSize.A4,10f,10f,10f,10f);
+            pdfdocument.SetPageSize(PageSize.A4.Rotate());
+            PdfWriter.GetInstance(pdfdocument, new FileStream(Server.MapPath("~/PDFdocument/Employee.pdf"),FileMode.Create));
+
+            pdfdocument.Open();
+            pdfdocument.Add(pdfPTable);
+            pdfdocument.Close();
+
+            //download pdf file...........
+            //Response.ContentType = "application/pdf";
+            //Response.AppendHeader("content-disposition","attachment;filename=Employees.pdf");
+            //Response.Write(pdfdocument);
+            //Response.Flush();
+            //Response.End();
+        }
+    }
+}
