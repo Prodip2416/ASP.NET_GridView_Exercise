af5c5dd685ca09dde7ecd12eb3cef6a74d72395f
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:26:27 +0600

    Grid View

---
+++ Example/DefaultPagingObjectDataSource/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DefaultPagingObjectDataSource
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_PreRender(object sender, EventArgs e)
+        {
+            Label1.Text = "Displaying page " + (GridView1.PageIndex + 1).ToString() + " of " +
+                          GridView1.PageCount.ToString();
+        }
+    }
+}
