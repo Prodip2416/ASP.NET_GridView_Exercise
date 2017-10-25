1e8606df89d36720fcc787021477ae589f536e32
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 00:16:05 +0600

    Grid View

---
+++ Example/DisplayingGridViewInGridView/DepartmentDataAccessLayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DisplayingGridViewInGridView
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public List<Employee> Employees
+        {
+            get
+            {
+                return EmployeeDataAccessLayer.GetAllEmployees(this.DepartmentId);
+            }
+        }
+    }
+    public class DepartmentDataAccessLayer
+    {
+        public static List<Department> GetAllDepartmentsandEmployees()
+        {
+            List<Department> listDepartments = new List<Department>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Department1", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Department department = new Department();
+                    department.DepartmentId = Convert.ToInt32(rdr["DepartmentId"]);
+                    department.DepartmentName = rdr["Name"].ToString();
+
+                    listDepartments.Add(department);
+                }
+            }
+
+            return listDepartments;
+        }
+    }
+}
---
+++ Example/DisplayingGridViewInGridView/EmployeeDataAccessLayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DisplayingGridViewInGridView
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+    public class EmployeeDataAccessLayer
+    {
+        public static List<Employee> GetAllEmployees(int DepartmentId)
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employee6 where DeptId = @DepartmentId", con);
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@DepartmentId";
+                parameter.Value = DepartmentId;
+                cmd.Parameters.Add(parameter);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.EmployeeName = rdr["Name"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return listEmployees;
+        }
+    }
+}
