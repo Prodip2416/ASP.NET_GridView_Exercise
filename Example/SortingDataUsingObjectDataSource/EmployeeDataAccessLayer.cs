122e35ead4e68bfe410ea3684fd7a58807126a93
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:05:09 +0600

    Grid View

---
+++ Example/SortingDataUsingObjectDataSource/EmployeeDataAccessLayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SortingDataUsingObjectDataSource
+{
+    public class EmployeeDataAccessLayer
+    {
+        public static DataSet GetAllEmployees()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Select * from tblEmployee", con);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                return ds;
+            }
+        }
+    }
+}
