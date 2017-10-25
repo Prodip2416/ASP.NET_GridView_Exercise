76188a64d79604e0ae2065e4878d9b4f931835dd
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:34:31 +0600

    Grid View

---
+++ Example/ImplementingDefaultPagingWithoutDS/EmployeeDataAccesslayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ImplementingDefaultPagingWithoutDS
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+    public class EmployeeDataAccesslayer
+    {
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employee4", con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+            return listEmployees;
+        }
+    }
+}
---
+++ Example/ImplementingDefaultPagingWithoutDS/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImplementingDefaultPagingWithoutDS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridView1.DataSource = EmployeeDataAccesslayer.GetAllEmployees();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+           
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataSource = EmployeeDataAccesslayer.GetAllEmployees();
+            GridView1.DataBind();
+        }
+    }
+}
