cd71d720500fde1d7aae0781305f0e7738003b06
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-10-26 01:18:57 +0600

    Grid View

---
+++ Example/SortArrowWhnSprtingGridView/EmployeeDataAccessLayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SortingGridViewWithOutDataSource
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+    public class EmployeeDataAccessLayer
+    {
+        public static List<Employee> GetAllEmployees(string sortColumn)
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                string sqlQuery = "Select * from Employee4";
+
+                if (!string.IsNullOrEmpty(sortColumn))
+                {
+                    sqlQuery += " order by " + sortColumn;
+                }
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    listEmployees.Add(employee);
+                }
+            }
+
+            return listEmployees;
+        }
+    }
+}
---
+++ Example/SortArrowWhnSprtingGridView/WebForm1.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SortingGridViewWithOutDataSource
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridView1.DataSource = EmployeeDataAccessLayer.GetAllEmployees("EmployeeId");
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            SortDirection sortDirection = SortDirection.Ascending;
+            string sortField = string.Empty;
+
+            SortGridview((GridView)sender, e, out sortDirection, out sortField);
+            string strSortDirection = sortDirection == SortDirection.Ascending ? "ASC" : "DESC";
+
+
+            GridView1.DataSource = EmployeeDataAccessLayer.GetAllEmployees
+                (e.SortExpression + " " + strSortDirection);
+            GridView1.DataBind();
+        }
+        private void SortGridview(GridView gridView, GridViewSortEventArgs e, out SortDirection sortDirection, out string sortField)
+        {
+            sortField = e.SortExpression;
+            sortDirection = e.SortDirection;
+
+            if (gridView.Attributes["CurrentSortField"] != null && gridView.Attributes["CurrentSortDirection"] != null)
+            {
+                if (sortField == gridView.Attributes["CurrentSortField"])
+                {
+                    if (gridView.Attributes["CurrentSortDirection"] == "ASC")
+                    {
+                        sortDirection = SortDirection.Descending;
+                    }
+                    else
+                    {
+                        sortDirection = SortDirection.Ascending;
+                    }
+                }
+
+                gridView.Attributes["CurrentSortField"] = sortField;
+                gridView.Attributes["CurrentSortDirection"] = (sortDirection == SortDirection.Ascending ? "ASC" : "DESC");
+            }
+        }
+
+        protected void GridView1_RowCreated(object sender, GridViewRowEventArgs e)
+        {
+            if (GridView1.Attributes["CurrentSortField"] != null && GridView1.Attributes["CurrentSortDirection"] != null)
+            {
+                if (e.Row.RowType == DataControlRowType.Header)
+                {
+                    foreach (TableCell tableCell in e.Row.Cells)
+                    {
+                        if (tableCell.HasControls())
+                        {
+                            LinkButton sortLinkButton = null;
+                            if (tableCell.Controls[0] is LinkButton)
+                            {
+                                sortLinkButton = (LinkButton)tableCell.Controls[0];
+                            }
+
+                            if (sortLinkButton != null && GridView1.Attributes["CurrentSortField"] == sortLinkButton.CommandArgument)
+                            {
+                                Image image = new Image();
+                                if (GridView1.Attributes["CurrentSortDirection"] == "ASC")
+                                {
+                                    image.ImageUrl = "~/Images/Down Filled-50.png";
+                                }
+                                else
+                                {
+                                    image.ImageUrl = "~/Images/Long Arrow Up-50.png";
+                                }
+                                tableCell.Controls.Add(new LiteralControl("&nbsp;"));
+                                tableCell.Controls.Add(image);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
